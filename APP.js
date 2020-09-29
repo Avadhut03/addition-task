2d52421ec8837accb16d5e96160b3e0348719a1a
Avadhut03 <avadhutkul60@gmail.com>
2020-09-29 12:55:00 +0530

    https://github.com/Avadhut03/addition-task

---
+++ APP.js
@@ -0,0 +1,24 @@
+var q = 200
+var p = 100
+
+var c = q + p
+
+console.log(c)
+var q = 200
+var p = 100
+
+var d = q - p
+
+console.log(d)
+var q = 200
+var p = 100
+
+var e = q * p
+
+console.log(e)
+var q = 200
+var p = 100
+
+var f = q / p
+
+console.log(f)
